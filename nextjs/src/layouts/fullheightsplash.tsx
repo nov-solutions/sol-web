72f4de752d11e488c260cef36843e5a0ad7040d9
Hunter <hunterc1011@gmail.com>
2024-04-30 21:09:23 -0500

    fix fullheightsplash layout

--- nextjs/src/layouts/fullheightsplash.tsx
+++ nextjs/src/layouts/fullheightsplash.tsx
@@ -24,7 +24,7 @@ export default function FullHeightSplash({
 
   return (
     <div className={"flex-grow mt-[7.5vh] px-4 lg:px-0 " + splashBG}>
-      <div className="grid items-center h-full mx-auto lg:grid-cols-2 lg:w-2/3">
+      <div className="grid items-center h-full gap-16 mx-auto lg:grid-cols-2 lg:w-2/3">
         <div className="flex flex-col space-y-4 text-center lg:text-left">
           <img src={"/static/assets/img/logos/" + splashLogoFileName} alt={SITE_NAME + " logo"} className="w-1/12 mx-auto lg:mx-0" />
           <h1 className={"text-6xl font-bold leading-none tracking-tight " + splashTitleColor}>{splashTitle}</h1>
