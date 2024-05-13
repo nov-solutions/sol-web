e118cabad3f10c7e25a8e85504a7a3b1ab7e1f86
Hunter <hunterc1011@gmail.com>
2024-05-12 23:06:31 -0500

    improve team cards display

--- nextjs/src/components/team.tsx
+++ nextjs/src/components/team.tsx
@@ -20,7 +20,7 @@ export default function Team({
     const teamCardsHTML =
         team && teamCardNameColor && teamCardRoleColor
         ? team.map((teammate, i) => (
-            <div key={i} className={"flex flex-col items-center justify-center w-full p-4 text-center rounded-lg lg:p-8 " + teamCardBG}>
+            <div key={i} className={"flex flex-col items-center justify-center w-full h-full p-4 text-center rounded-lg lg:p-8 " + teamCardBG}>
                 <img src={"/static/assets/img/headshots/" + teammate.headshotLoc} alt={teammate.name + "'s headshot"} className="w-1/2 mx-auto rounded-full filter grayscale" />
                 <h2 className={"mt-4 text-sm font-medium " + teamCardNameColor}>{teammate.name}</h2>
                 <h3 className={"text-xs " + teamCardRoleColor}>{teammate.role}</h3>
@@ -32,7 +32,7 @@ export default function Team({
 
   return (
     <div className={"flex-grow px-4 py-12 lg:px-0 " + teamBG}>
-      <div className="grid items-center gap-4 mx-auto lg:grid-cols-4 lg:w-2/3">
+      <div className="grid items-stretch gap-4 mx-auto lg:grid-cols-4 lg:w-2/3">
         {teamCardsHTML}
       </div>
     </div>
