d2f3504e99d692539a3c21253e933eeec9b95767
hunterchemelli <hunterc1011@gmail.com>
2024-07-19 00:17:37 -0500

    fix access to django admin static files

--- django/newsolwebapp/settings.py
+++ django/newsolwebapp/settings.py
@@ -21,13 +21,11 @@ elif ENVIRONMENT == "prod":
     DEBUG = False
     config._load(".prod.env")
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 # TODO: uncomment if project uses auth
 # AUTH_USER_MODEL = SITE_NAME + ".User"
 
 STATIC_URL = "/api/static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = "/static/"
 
 ASGI_APPLICATION = SITE_NAME + ".asgi.application"
 
