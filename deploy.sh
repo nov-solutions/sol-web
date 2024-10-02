c2f5b97daf94f196b1ace2dd985bbc42d379cd32
thatcher thornberry <thatcherthornberry@gmail.com>
2024-10-02 12:30:17 -0600

    pipe

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-cd ./nextjs
-npx tailwindcss -i ./public/static/css/input.css -o ./public/static/css/styles.css --minify
-cd ../
-
-rsync -r --delete --progress --exclude="node_modules" --exclude="nextjs/.next" --exclude="nextjs/node_modules" --exclude=".venv" --exclude=".git" --exclude="staticfiles" -e "ssh -i NEWSOLWEBAPP-web.pem" . ubuntu@TODOipaddress:app
