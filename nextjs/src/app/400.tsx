2cb18bb26d1b036380b6dfcc434fb5224c111989
Hunter <hunterc1011@gmail.com>
2024-11-04 19:10:23 -0600

    change twitter card display to summary_large_image

--- nextjs/src/app/400.tsx
+++ nextjs/src/app/400.tsx
@@ -47,7 +47,7 @@ export const metadata: Metadata = {
     type: "website",
   },
   twitter: {
-    card: "summary",
+    card: "summary_large_image",
     title: title,
     description: SITE_DESCRIPTION,
     images: [
