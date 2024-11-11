10ead079bbd1fde0f67ba7eab582ba4a86a167cb
thatcher thornberry <thatcherthornberry@gmail.com>
2024-11-11 13:14:45 -0700

    swag back

--- django/sol/urls.py
+++ django/sol/urls.py
@@ -1,5 +1,36 @@
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 from django.contrib import admin
 from django.urls import path
+
+# Swagger patterns
+swagger_patterns = [
+    path(
+        "swagger.json",  # This explicitly handles the JSON schema
+        SpectacularAPIView.as_view(),
+        name="schema-json",
+    ),
+    path(
+        "swagger.yaml",  # This explicitly handles the YAML schema
+        SpectacularAPIView.as_view(),
+        name="schema-yaml",
+    ),
+    path(
+        "swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema-json"),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "redoc/",
+        SpectacularRedocView.as_view(url_name="schema-json"),
+        name="schema-redoc",
+    ),
+]
+
 
 urlpatterns = [
     path("api/admin/", admin.site.urls),
