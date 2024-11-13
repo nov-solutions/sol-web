33177f8da4412eaf6353027abd00ccf67b4981fd
thatcher thornberry <thatcherthornberry@gmail.com>
2024-11-13 10:35:39 -0700

    BASES

--- django/sol/urls.py
+++ django/sol/urls.py
@@ -1,37 +1,12 @@
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularRedocView,
-    SpectacularSwaggerView,
-)
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
-# Swagger patterns
-swagger_patterns = [
-    path(
-        "swagger.json",  # This explicitly handles the JSON schema
-        SpectacularAPIView.as_view(),
-        name="schema-json",
-    ),
-    path(
-        "swagger.yaml",  # This explicitly handles the YAML schema
-        SpectacularAPIView.as_view(),
-        name="schema-yaml",
-    ),
-    path(
-        "swagger/",
-        SpectacularSwaggerView.as_view(url_name="schema-json"),
-        name="schema-swagger-ui",
-    ),
-    path(
-        "redoc/",
-        SpectacularRedocView.as_view(url_name="schema-json"),
-        name="schema-redoc",
-    ),
-]
+from .urls_base import swagger_patterns
 
+api_patterns = []
 
 urlpatterns = [
+    path("api/", include(api_patterns)),
     path("api/admin/", admin.site.urls),
+    path("api/docs/", include(swagger_patterns)),
 ]
---
+++ django/sol/urls_base.py
@@ -0,0 +1,30 @@
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+from django.urls import path
+
+swagger_patterns = [
+    path(
+        "swagger.json",  # This explicitly handles the JSON schema
+        SpectacularAPIView.as_view(),
+        name="schema-json",
+    ),
+    path(
+        "swagger.yaml",  # This explicitly handles the YAML schema
+        SpectacularAPIView.as_view(),
+        name="schema-yaml",
+    ),
+    path(
+        "swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema-json"),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "redoc/",
+        SpectacularRedocView.as_view(url_name="schema-json"),
+        name="schema-redoc",
+    ),
+]
