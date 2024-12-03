691d973e71f11c559208ffe8c2a3980855b16837
Hunter <hunterc1011@gmail.com>
2024-12-02 19:25:24 -0600

    improve tagline

--- nextjs/src/constants.ts
+++ nextjs/src/constants.ts
@@ -3,7 +3,7 @@ export const SITE_NAME = String(process.env.NEXT_PUBLIC_SITE_NAME).replace(
   (char) => char.toUpperCase(),
 );
 // TODO: replace with the project's tagline
-export const SITE_TAGLINE = "All-in-one web app template";
+export const SITE_TAGLINE = "All-in-One Web App Template";
 // TODO: replace with the project's description
 export const SITE_DESCRIPTION =
   "Sol is an all-in-one template that enables developers to create robust, reliable and responsive web apps in minutes.";
