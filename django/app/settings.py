66acc02c1e354c153cb5c03eed88e6f645e3698e
thatcher thornberry <thatcherthornberry@gmail.com>
2024-12-20 18:24:45 -0700

    f

--- django/app/settings.py
+++ django/app/settings.py
@@ -64,7 +64,7 @@ DATABASES = {
         "NAME": POSTGRES_DB,
         "USER": POSTGRES_USER,
         "PASSWORD": POSTGRES_PASSWORD,
-        "HOST": "127.0.0.1",
+        "HOST": "postgres",
         "PORT": 5432,
     }
 }
