659c9012401c1d7984ea303fb49c6f7d641f89e0
thatcher thornberry <thatcherthornberry@gmail.com>
2025-02-24 15:28:48 -0700

    drop pwa support

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,19 +1,3 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  webpackDevMiddleware: (config) => {
-    console.log("Webpack Watch Options:", config.watchOptions);
-    config.watchOptions = {
-      poll: 1000, // Check for changes every second
-      aggregateTimeout: 300, // Delay before rebuilding
-    };
-    return config;
-  },
-};
-
-const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: process.env.NODE_ENV == "development",
-});
-
-module.exports = withPWA(nextConfig);
+const nextConfig = {};
