b5a8189f3800ff70934cbc6601d67bd86f223693
thatcher thornberry <thatcherthornberry@gmail.com>
2025-02-24 23:46:48 -0700

    software version upgrades

--- django/celeryapp/celery.py
+++ django/celeryapp/celery.py
@@ -1,11 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 
-import os
-
 from celery import Celery
 from celeryapp import celery_config
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 app = Celery("web")
 
