f454a665c2725c6918ad50453eab4bf8cd86fbd6
thatcher thornberry <thatcherthornberry@gmail.com>
2025-06-06 14:08:15 -0600

    working

--- web/celeryapp/celery_config.py
+++ web/celeryapp/celery_config.py
@@ -3,10 +3,11 @@ import os
 from kombu import Exchange
 
 REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD")
+REDIS_HOST = os.environ.get("REDIS_HOST", "redis")
 
 
-broker_url = f"redis://:{REDIS_PASSWORD}@redis:6379/0"
-result_backend = f"redis://:{REDIS_PASSWORD}@redis:6379/0"
+broker_url = f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:6379/0"
+result_backend = f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:6379/0"
 result_expires = 60 * 60 * 24  # 24 hours
 
 task_default_queue = "default"
@@ -17,7 +18,7 @@ task_default_priority = 5
 broker_connection_retry_on_startup = True
 broker_connection_max_retries = 10
 
-redbeat_redis_url = f"redis://:{REDIS_PASSWORD}@redis:6379/1"
+redbeat_redis_url = f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:6379/1"
 
 default_exchange = Exchange("default", type="topic")
 
